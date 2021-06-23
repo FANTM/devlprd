8daf4358fd369f9a2f752d0ce35d19fc9b24ea94
Ezra Boley <eboley@wisc.edu>
2021-06-23 16:16:22 -0500

    Added a test for publishing/subbing to a topic

--- tests/ws_test.py
+++ tests/ws_test.py
@@ -1,21 +1,32 @@
 import os, sys, time
 from threading import Thread
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from devlprd import *
 from protocol import *
 
 async def test_connect():
-    print("Start")
+    print("Start test_connect()")
     try:
         async with websockets.connect("ws://{}:{}".format(ADDRESS[0], ADDRESS[1])) as ws:
             await ws.send("{}{}{}".format(PacketType.SUBSCRIBE, DELIM, DataTopic.RAW_DATA_TOPIC))
+            time.sleep(1)
+            await pub(DataTopic.RAW_DATA_TOPIC, 0, "test")
             data = await ws.recv()
             print(data)
+            broken_packet = data.split(DELIM, maxsplit=3)
+            assert len(broken_packet) == 3
+            (topic, pin, meat) = broken_packet
+            assert topic == DataTopic.RAW_DATA_TOPIC
+            assert int(pin) == 0
+            assert meat == "test"
+            print("PASS test_connect()")
     except Exception:
         raise KeyboardInterrupt
 
 async def run_all_tests():
     await test_connect()
+    print("Testing complete, Ctrl-C to leave...")
 
 if __name__ == "__main__":
     try:
