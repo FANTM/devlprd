46f45cd150afc5f1f26947285a9f7b11b60365ca
Ezra Boley <eboley@wisc.edu>
2021-07-15 15:04:50 -0500

    Reorganized file structure, added tests, made the serif OOP, and did some general reworking

--- devlprd/DaemonState.py
+++ devlprd/DaemonState.py
@@ -1,20 +1,29 @@
-from typing import List, Dict, Deque
-from websockets import server
-from threading import Lock
+import asyncio
 import logging
-from collections import deque
-from protocol import wrap_packet
+import threading
+import collections as coll
+from typing import Callable, Deque, Dict, List, Union
+import websockets.server
+import filtering
+import protocol
+
 
 ## Thread protected shared state for the Daemon. It manages all of the connections and data topics
 class DaemonState:
     BUFFER_SIZE = 155
     def __init__(self):
-        self.SUBS: Dict[str, List[server.WebSocketServerProtocol]] = dict()
+        self.SUBS: Dict[str, List[websockets.server.WebSocketServerProtocol]] = dict()
         self.SERIAL_DATA: Dict[int, Deque[int]] = dict()
-        self.SUBS_LOCK = Lock()
-        self.SERIAL_DATA_LOCK = Lock()
+        self.SUBS_LOCK = threading.Lock()
+        self.SERIAL_DATA_LOCK = threading.Lock()
+        self.event_loop: asyncio.events.AbstractEventLoop = None
+        self.callbacks: Dict[str, Callable[[int], Union[int, bool]]] = {
+            protocol.DataTopic.RAW_DATA_TOPIC : self.peek_serial_data,
+            protocol.DataTopic.FLEX_TOPIC     : self.flex_callback,
+        }
+        self.server: websockets.server.WebSocketServer = None
 
-    def subscribe(self, websocket: server.WebSocketServerProtocol, topic: str) -> None:
+    def subscribe(self, websocket: websockets.server.WebSocketServerProtocol, topic: str) -> None:
         with self.SUBS_LOCK:
             try:
                 self.SUBS[topic].append(websocket)
@@ -22,34 +31,39 @@ class DaemonState:
                 self.SUBS[topic] = list()
                 self.SUBS[topic].append(websocket)
     
-    async def pub(self, topic: str, pin: int, data: str) -> None:
+    def pub(self, pin: int) -> None:
+        # attributes = [attr for attr in dir(DataTopic) if not attr.startswith('__')]  # https://stackoverflow.com/a/5970022
+        for topic, cb in self.callbacks.items():
+            asyncio.run_coroutine_threadsafe(self._pub(topic, pin, cb), loop=self.event_loop)
+
+    async def _pub(self, topic: str, pin: int, callback: Callable[[int], Union[int, bool]]) -> None:
         try:
             for sub in self.SUBS[topic]:
-                await sub.send(wrap_packet(topic, pin, data))
+                await sub.send(protocol.wrap_packet(topic, pin, callback(pin)))
         except:
             pass
 
-    def unsubscribe(self, websocket: server.WebSocketServerProtocol, topic: str) -> None:
+    def unsubscribe(self, websocket: websockets.server.WebSocketServerProtocol, topic: str) -> None:
         try:
             with self.SUBS_LOCK:
                 self.SUBS[topic].remove(websocket)
         except ValueError:
             logging.warning("Trying to unsubscribe w/o ever subscribing")
 
-    def unsubscribe_all(self, websocket: server.WebSocketServerProtocol) -> None:
+    def unsubscribe_all(self, websocket: websockets.server.WebSocketServerProtocol) -> None:
         with self.SUBS_LOCK:
             for subscribers in self.SUBS.values():
                 try:
                     subscribers.remove(websocket)
                 except ValueError:
                     pass
-    
+
     def push_serial_data(self, pin: int, data: int) -> None:
         with self.SERIAL_DATA_LOCK:
             try:
                 self.SERIAL_DATA[pin].appendleft(data)
             except KeyError:
-                self.SERIAL_DATA[pin] = deque(maxlen=self.BUFFER_SIZE)
+                self.SERIAL_DATA[pin] = coll.deque(maxlen=self.BUFFER_SIZE)
                 self.SERIAL_DATA[pin].appendleft(data)
 
     def pop_serial_data(self, pin: int) -> int:
@@ -68,3 +82,7 @@ class DaemonState:
                 raise e
             except ValueError:
                 logging.info("Buffer empty")
+                return -1
+    
+    def flex_callback(self, pin: int) -> bool:
+        return filtering.flex_check(self.peek_serial_data(pin))
