21d0bcdf55d5fd9dd40898cae8e8b89505dde874
Ezra Boley <ezra@getfantm.com>
2021-11-16 17:54:09 -0600

    Updated tests

--- tests/serial_test.py
+++ tests/serial_test.py
@@ -1,12 +1,13 @@
 import time, pytest, asyncio, logging
 from ..src.devlprd.DaemonState import DaemonState
 from ..src.devlprd import serif
+from ..src.devlprd.config import BOARDS
 
-STATE: DaemonState = DaemonState(asyncio.get_event_loop())
+STATE: DaemonState = DaemonState(asyncio.get_event_loop(), BOARDS['DEVLPR'])
 logging.basicConfig(level=logging.INFO)
 class TestSerial():
     def test_basic(self):
-        DEVLPR_SERIF = serif.DevlprSerif()
+        DEVLPR_SERIF = serif.DevlprSerif(BOARDS['DEVLPR'])
         DEVLPR_SERIF.init_serial(STATE)
         i = 0
         while i < 500:
